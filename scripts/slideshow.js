7f9b9001c689a0d7927db558f0f44a82e7cfac82
debarati-datta <debarati.datta@outlook.com>
2020-09-13 09:41:58 +0000

    adds google map

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -23,10 +23,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-2].style.display = "block";
   dots[slideIndex-1].className += " active";
-};
+}
 
-$(function(){
-    $("#accordion").accordion();
-  });

75a375127bd30e34248f70f1bacfc65a2701fa2c
debarati-datta <debarati.datta@outlook.com>
2020-09-17 21:45:15 +0000

    updates form and food section on accordion

--- scripts/maps.js
+++ scripts/maps.js
@@ -2,22 +2,22 @@ function initMap() {
             var map = new google.maps.Map(document.getElementById("map"), {
                 zoom: 3,
                 center: {
-                    lat: 46.619261,
-                    lng: -33.134766
+                    lat: -21.449123,
+                    lng: 81.716711
                 }
             });
 
             var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 
             var locations = [{
-                lat: 40.785091,
-                lng: -73.968285
+                lat: 26.2313967,
+                lng: 78.1683799
             }, {
-                lat: 41.084045,
-                lng: -73.874256
+                lat: -26.231395,
+                lng: 78.169516
             }, {
-                lat: 40.754932,
-                lng: -73.984016
+                lat: -24.853174,
+                lng: 79.919724
             }];
 
             var markers = locations.map(function(location, i) {

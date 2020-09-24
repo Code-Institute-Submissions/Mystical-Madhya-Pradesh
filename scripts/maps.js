0766e3d54ccf76e8df790123c96e454fce5e02a8
debarati-datta <debarati.datta@outlook.com>
2020-09-24 21:42:00 +0000

    google map shows holiday destinations, refines media query for homepage

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,33 +1,29 @@
 function initMap() {
             var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
-                center: {
-                    lat: -21.449123,
-                    lng: 81.716711
-                }
+               
+                center: {lat: 20.5937, lng: 78.9629},
+                 zoom: 5
             });
 
-            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+            
+            addMarker({lat: 26.2183,lng: 78.1828});
+            addMarker({lat: 24.8318,lng: 79.9199});
+            addMarker({lat: 22.4674,lng: 78.4346});
+            addMarker({lat: 23.1815,lng: 79.9864});
+            addMarker({lat: 21.6495,lng: 79.2451});
+            addMarker({lat: 22.3345,lng: 80.6115});    
 
-            var locations = [{
-                lat: 26.2313967,
-                lng: 78.1683799
-            }, {
-                lat: -26.231395,
-                lng: 78.169516
-            }, {
-                lat: -24.853174,
-                lng: 79.919724
-            }];
 
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
+            function addMarker(coords){
+                var marker = new google.maps.Marker({
+                    position:coords,
+                    map:map
                 });
-            });
-
-            var markerCluster = new MarkerClusterer(map, markers, {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-            });
+            }
+            
         }
+
+            
+                
+                
+               

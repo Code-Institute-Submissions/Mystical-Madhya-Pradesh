a707f3f316313da4a12743a1912da44f3a3b0eb9
debarati-datta <debarati.datta@outlook.com>
2020-10-09 06:46:37 +0000

    accordion working fixed font-awesome version discrepency

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,14 +1,15 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
-
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+        //accordion is open and we need to close it
+      content.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+        //accordion is closed
+      content.style.maxHeight = content.scrollHeight + "px";
     } 
   });
 };
